65d7233fd63dd87844e2cc7a8e09cf73376e9c97
RobPavl <robert.pavlovsky@gmail.com>
2021-01-14 18:21:23 +0200

    Add relations for categories and posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_150031) do
+ActiveRecord::Schema.define(version: 2021_01_14_155400) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2021_01_14_150031) do
     t.text "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
+  add_foreign_key "posts", "categories"
 end
