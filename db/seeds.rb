3ad210bc325cc3cbae245a42127055e24331966c
RobPavl <robert.pavlovsky@gmail.com>
2021-01-23 19:40:23 +0200

    Add seeds for categories.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+category_list = [
+  'First Category',
+  'Second Category',
+  'Third Category'
+]
+
+category_list.each do |name|
+  Category.create( name: name )
+end
